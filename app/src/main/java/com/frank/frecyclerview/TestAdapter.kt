0af684f0080b52c72a684febd06c59323ae1a652
frankchoo <frankchoochina@gmail.com>
2018-08-03 16:43:16 +0800

    添加了使用相关的 Demo

---
+++ app/src/main/java/com/frank/frecyclerview/TestAdapter.kt
@@ -0,0 +1,27 @@
+package com.frank.frecyclerview
+
+import android.content.Context
+import android.view.View
+import android.widget.Toast
+import com.frank.lib_recyclerview.CommonRecyclerAdapter
+import com.frank.lib_recyclerview.CommonViewHolder
+
+/**
+ * Created by Frank on 2018/8/3.
+ * Email: frankchoochina@gmail.com
+ * Version: 1.0
+ * Description:
+ */
+class TestAdapter(context: Context, dataSet: MutableList<String>)
+    : CommonRecyclerAdapter<String>(context, dataSet) {
+
+    override fun getLayoutRes(data: String, position: Int): Int = R.layout.recycler_item_test
+
+    override fun convert(holder: CommonViewHolder, data: String, position: Int) {
+        holder.setText(R.id.tv_item_desc, data)
+    }
+
+    override fun onItemClick(v: View, position: Int) {
+        Toast.makeText(v.context, "$position is clicked.", Toast.LENGTH_SHORT).show()
+    }
+}
